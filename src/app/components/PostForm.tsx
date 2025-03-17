f35a4479da3d8dbc333b7a63df56f1197e653559
vicentferrer <vicent.ferrer@idrica.com>
2025-03-17 23:17:45 +0100

    Formulario de creación de posts

--- src/app/components/PostForm.tsx
+++ src/app/components/PostForm.tsx
@@ -1,21 +1,48 @@
 import Button from "./Button"
 import "../../styles/postform.css";
+import { useState } from "react";
+import { Post } from "../types/types";
 
-
-function PostForm() {
-  
-    
-
-  return (
-    <div>
-        <form action="">
-        <input type="text" name="" id="" />
-        <textarea></textarea>
-        
-    <Button text="Enviar" onClick={() => alert("Hola!")} />
-    </form>
-    </div>
-  )
+interface PostFormProps {
+  initialPost?: Post;
+  onSubmit: (post: Post) => void;
 }
 
-export default PostForm
+function PostForm({ initialPost, onSubmit }: PostFormProps){
+
+  const [post, setPost] = useState<Post>({
+    id: initialPost?.id || 0,
+    title: initialPost?.title || "",
+    content: initialPost?.content || ""
+  });
+
+
+  const handleSubmit = (e : React.FormEvent) => {
+    e.preventDefault();
+    console.log(post)
+    onSubmit(post);
+  }
+
+  const handleTitle = (e: React.FormEvent) => {
+    setPost({...post, title: e.target.value})
+  }
+
+  const handleContent = (e: React.FormEvent) => {
+    setPost({...post, content: e.target.value})
+  }
+
+
+  return (
+    <div className="post-container">
+    <form className="form-container" onSubmit={handleSubmit}>
+      <label htmlFor="title">Title</label>
+      <input type="text" id="title" value={post.title} placeholder="Set Title" onChange={handleTitle} />
+      <label htmlFor="content">Content</label>
+      <textarea  id="content" value={post.content} placeholder="Set Content" onChange={handleContent}></textarea>
+      <Button text="Create" onClick={() => handleSubmit}  />
+    </form>
+    </div>
+  );
+};
+
+export default PostForm;
--- src/app/pages/CreatePage.tsx
+++ src/app/pages/CreatePage.tsx
@@ -1,15 +1,33 @@
-import Header from "../components/header/Header"
-import PostForm from "../components/PostForm"
+import { useState } from "react";
+import Header from "../components/header/Header";
+import PostForm from "../components/PostForm";
+import { Post } from "../types/types";
 
 
 function CreatePage() {
+  const [newPost, setNewPost] = useState<Post | null>(null);
+
+  const handleCreateSubmit = (post) => {
+    setNewPost(post);
+  };
+
   return (
     <div>
-        <Header />
-      <h1>Este es un formulario de creación de un nuevo post</h1>
-      <PostForm></PostForm>
+       <Header />
+      <h1>Create a new Post</h1>
+      <PostForm onSubmit={handleCreateSubmit} />
+      {newPost && (
+        <div>
+          <h2>{newPost.id}</h2>
+          <h2>{newPost.title}</h2>
+          <h2>{newPost.content}</h2>
+        </div>
+      )
+
+      }
     </div>
   )
 }
 
 export default CreatePage
+
