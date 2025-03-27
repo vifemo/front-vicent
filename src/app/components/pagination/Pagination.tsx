70779baacde2caffeeafb32147aeb7ae337e8909
vicentferrer <vicent.ferrer@idrica.com>
2025-03-27 11:34:54 +0100

    componente pagination

---
+++ src/app/components/pagination/Pagination.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+interface PaginationProps {
+  currentPage: number
+  totalPages: number
+  onPageChange: (page: number) => void
+}
+
+const Pagination: React.FC<PaginationProps> = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+}) => {
+  return (
+    <div className="pagination">
+      <button
+        onClick={() => onPageChange(currentPage - 1)}
+        disabled={currentPage === 1}
+      >
+        Prev
+      </button>
+
+      {[...Array(totalPages)].map((_, i) => (
+        <button key={i} onClick={() => onPageChange(i + 1)}>
+          {i + 1}
+        </button>
+      ))}
+
+      <button></button>
+      <button
+        onClick={() => onPageChange(currentPage + 1)}
+        disabled={currentPage === totalPages}
+      >
+        Next
+      </button>
+    </div>
+  )
+}
+
+export default Pagination
